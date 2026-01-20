a2d87a510e86034dac66ab9144acb4d6fdf447cc
Aadie2209 <soodaadie@gmail.com>
2026-01-21 01:01:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Website loaded for Mannuu 💗");
+const songs = ["music1.mp3", "music2.mp3"];
+let currentSong = 0;
+
+const audio = new Audio(songs[currentSong]);
+audio.volume = 0.5;
+audio.play();
+
+audio.addEventListener("ended", () => {
+  currentSong++;
+  if (currentSong < songs.length) {
+    audio.src = songs[currentSong];
+    audio.play();
+  }
+});
