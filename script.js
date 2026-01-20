bad263ce187432553b970dd7db2c64d359e74c1e
Aadie2209 <soodaadie@gmail.com>
2026-01-21 01:30:44 +0530

    Implement music play button functionality
    Added a button to play music when clicked.

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ let currentSong = 0;
 
 const audio = new Audio(songs[currentSong]);
 audio.volume = 0.5;
-audio.play();
+
+const musicBtn = document.getElementById("musicBtn");
+if (musicBtn) {
+  musicBtn.addEventListener("click", () => {
+    audio.play();
+  });
+}
 
 audio.addEventListener("ended", () => {
   currentSong++;
